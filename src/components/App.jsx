1b93f6a2c5aeaf2a0d28296bddd028998bbc4cf0
VitaliiKovalenko88 <vintilyator88@gmail.com>
2023-11-09 23:31:37 +0100

    add task 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,51 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        textTransform: 'uppercase',
-        color: '#010101',
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  countFeedback = e => {
+    const { textContent } = e.target;
+    this.setState(prevState => {
+      return { [textContent]: prevState[textContent] + 1 };
+    });
+  };
+
+  render() {
+    return (
+      <section>
+        <div>
+          <h2>Please leave feedback</h2>
+          <ul>
+            <li>
+              <button type="button" onClick={this.countFeedback}>
+                Good
+              </button>
+            </li>
+            <li>
+              <button type="button" onClick={this.countFeedback}>
+                Neutral
+              </button>
+            </li>
+            <li>
+              <button type="button" onClick={this.countFeedback}>
+                Bad
+              </button>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h2>Statistics</h2>
+          <ul>
+            <li>Good: {this.state.good}</li>
+            <li>Neutral: {this.state.neutral}</li>
+            <li>Bad: {this.state.bad}</li>
+          </ul>
+        </div>
+      </section>
+    );
+  }
+}
